0e7a838a2523a7de0303df343cbb1e4cff25aeff
Jonathan <blanchard.jonathan@gmail.com>
2016-04-20 20:34:08 -0400

    Created a 5x5 grid of buttons and a Button array in the java code to refer to each button.

--- app/src/main/java/testapp/com/gemgame/game.java
+++ app/src/main/java/testapp/com/gemgame/game.java
@@ -3,6 +3,7 @@ package testapp.com.gemgame;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class game extends AppCompatActivity {
@@ -11,7 +12,20 @@ public class game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        
+
+        Button buttons[][] = new Button[5][5];
+
+        for (int col = 0; col < 5; col++) {
+            for (int row = 0; row < 5; row++) {
+                String buttonID = "button" + col + row;
+                int resID = getResources().getIdentifier(buttonID, "id", this.getPackageName());
+                buttons[col][row] = (Button) findViewById(resID);
+            }
+        }
+
+        buttons[0][0].setText("1");
+        buttons[2][3].setText("4");
+
 /* this is now old code
         String[] testArray = {"1", "3", "5"};
 
